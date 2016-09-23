d208769f1c360d89b22fbf1c33141e634cbe1b7b
Vikram Tiwari <vikramtheone1@gmail.com>
2016-09-22 22:56:39 -0700

    outlier cleaner

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,8 +25,13 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
+from sklearn.linear_model import LinearRegression
 
+reg = LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
+print "slope:", reg.coef_
+print "score:", reg.score(ages_test, net_worths_test)
 
 
 
@@ -69,6 +74,8 @@ if len(cleaned_data) > 0:
     try:
         reg.fit(ages, net_worths)
         plt.plot(ages, reg.predict(ages), color="blue")
+        print "slope:", reg.coef_
+        print "score:", reg.score(ages_test, net_worths_test)
     except NameError:
         print "you don't seem to have regression imported/created,"
         print "   or else your regression object isn't named reg"
@@ -81,4 +88,3 @@ if len(cleaned_data) > 0:
 
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
-
